f45d7fbc05989000c0b71c88671f170c0919c96c
LAPTOP-3EUHGI5A\100su <100sunilreddy@gmail.com>
2016-12-01 21:00:06 -0600

    java HW2

---
+++ hw2/BreakandContinue.java
@@ -0,0 +1,16 @@
+
+public class BreakandContinue {
+
+	public static void main(String[] args) {
+	 
+		for(int i=0;i<10;i++){
+			System.out.println(i);
+			if(i==5){
+				/*continue;*/
+				break;
+			}
+		
+		}
+	}
+
+}
---
+++ hw2/SwitchExample.java
@@ -0,0 +1,31 @@
+
+public class SwitchExample {
+
+	public static void main(String[] args) {
+	
+		int j=2;
+		
+		switch(j){
+		case 0:
+			System.out.println("value is 0");
+			break;
+		case 1:
+			System.out.println("value is 1");
+			break;
+		case 2:
+			System.out.println("value is 2");
+			break;
+		case 3:
+			System.out.println("value is 3");
+			break;
+		case 4:
+			System.out.println("value is 4");
+			break;
+		default:
+			System.out.println("No value");
+			break;
+			
+		}
+	}
+
+}

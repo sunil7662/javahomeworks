52d117f50227400de16911473aa84d2a4da3d671
LAPTOP-3EUHGI5A\100su <100sunilreddy@gmail.com>
2016-12-12 13:48:56 -0600

    hw3

---
+++ hw3/Immutable.java
@@ -0,0 +1,13 @@
+
+public class Immutable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1 ="sunil";
+		String s2 = s1.concat("reddy");
+		System.out.println(s1);
+		System.out.println(s2);
+		
+	}
+
+}
